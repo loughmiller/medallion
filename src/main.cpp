e7a130b36c9b4de7514ac76fe710a029176f9621
Scott Loughmiller <scott@scalecomputing.com>
2017-07-30 14:51:59 -0700

    Mostly working
    - Boots with random colors
    - After reboot, the colors cycle forever
    - Modify sparkle or spinner via touch, then new color will hold

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,11 @@
 // FAST LED
 #define NUM_LEDS 24
 #define DISPLAY_LED_PIN 22
+#define SPARKLE_CONTROL_PIN 1
+#define SPINNER_CONTROL_PIN 15
+#define FLASHLIGHT_CONTROL_PIN 23
+
+#define TOUCH_SENSITIVITY 700
 
 CRGB leds[NUM_LEDS];
 CRGB off;
@@ -17,48 +22,147 @@ void setAll(CRGB color);
 #define NUM_STREAKS 1
 #define NUM_SPARKELS 1
 
+#define SATURATION 244
+#define VALUE 255
+
 CRGB pink = 0xFF0B20;
 CRGB blue = 0x0BFFDD;
+CRGB white = 0xFFFFFF;
 
-CRGB colors[3];
+uint8_t spinnerHue;
+uint8_t sparkleHue;
+
+CHSV spinnerHSVColor;
+CRGB spinnerRGBColor;
+
+CHSV sparkleHSVColor;
+CRGB sparkleRGBColor;
+
 Spinner * spinner;
 Sparkle * sparkle;
+
+uint16_t interval = 50;
+uint32_t nextTime = 0;
+
+bool flashlight = false;
+bool spinnerColorCycle = true;
+bool sparkleColorCycle = true;
+
+#define SPINNER_SPEED 14009
+#define SPARKLE_SPEED 2333
+
+uint32_t randomTimeOffset;   // Just so we don't start with the same colors
+                              // every time
 
 void setup() {
   delay(2000);
 
-  // Serial.begin(9600);
-  // Serial.println("setup started");
+  Serial.begin(9600);
+  Serial.println("setup started");
+
+  randomSeed(analogRead(14));
 
   // DISPLAY STUFF
-  colors[1] = pink;
-  colors[2] = blue;
-
   off = 0x000000;
   FastLED.addLeds<NEOPIXEL, DISPLAY_LED_PIN>(leds, NUM_LEDS).setCorrection( Typical8mmPixel );;
 
-  FastLED.setBrightness(24);
-  setAll(0xFFFFFF);
+  FastLED.setBrightness(64);
+  setAll(white.fadeLightBy(244));
   FastLED.show();
   delay(2000);
 
   spinner = new Spinner(NUM_LEDS, leds, blue);
-
   sparkle = new Sparkle(1, NUM_LEDS, leds, pink, 450);
 
-  // Serial.println("setup complete");
+  randomTimeOffset = random(SPINNER_SPEED*SPARKLE_SPEED);
+  Serial.println(randomTimeOffset);
+  Serial.println("setup complete");
 }
 
 void loop() {
+  unsigned long currentTime = millis();
   clear();  // this only clears the array, not the LEDs, it's fine at the top
 
-  unsigned long currentTime = millis();
+  if (spinnerColorCycle) {
+    spinnerHue = ((currentTime + randomTimeOffset)/SPINNER_SPEED) % 256;
+    spinnerHSVColor = CHSV(spinnerHue, SATURATION, VALUE);
+    hsv2rgb_rainbow( spinnerHSVColor, spinnerRGBColor);
+    spinner->setColor(spinnerRGBColor);
+  }
+
+  if (sparkleColorCycle) {
+    sparkleHue = ((currentTime + randomTimeOffset)/SPARKLE_SPEED) % 256;
+    sparkleHSVColor = CHSV(sparkleHue, SATURATION, VALUE);
+    hsv2rgb_rainbow(sparkleHSVColor, sparkleRGBColor);
+    sparkle->setColor(sparkleRGBColor);
+  }
+
+  // Serial.print(touchRead(1));
+  // Serial.print("\t\");
+  // Serial.println(touchRead(15));
+
+  if (touchRead(FLASHLIGHT_CONTROL_PIN) > TOUCH_SENSITIVITY) {
+    flashlight = true;
+    setAll(0xFFFFFF);
+    FastLED.show();
+    delay(1000);
+  }
+
+  if (flashlight) {
+    if (touchRead(FLASHLIGHT_CONTROL_PIN) > TOUCH_SENSITIVITY ||
+        touchRead(SPINNER_CONTROL_PIN) > TOUCH_SENSITIVITY ||
+        touchRead(SPARKLE_CONTROL_PIN) > TOUCH_SENSITIVITY) {
+      flashlight = false;
+      setAll(0x000000);
+      FastLED.show();
+      delay(1000);
+    } else {
+      setAll(0xFFFFFF);
+      FastLED.show();
+      return;
+    }
+  }
+
+  if (touchRead(SPINNER_CONTROL_PIN) > TOUCH_SENSITIVITY) {
+    spinnerColorCycle = false;
+    if (currentTime > nextTime) {
+      // Serial.print(currentTime);
+      // Serial.print(", ");
+      // Serial.println(nextTime);
+      nextTime = currentTime + interval;
+      spinnerHue = (spinnerHue + 1) % 256;
+      spinnerHSVColor = CHSV(spinnerHue, SATURATION, VALUE);
+      hsv2rgb_rainbow( spinnerHSVColor, spinnerRGBColor);
+      spinner->setColor(spinnerRGBColor);
+    }
+
+    setAll(spinnerRGBColor);
+    FastLED.show();
+    return;
+  }
+
+  if (touchRead(SPARKLE_CONTROL_PIN) > TOUCH_SENSITIVITY) {
+    sparkleColorCycle = false;
+    if (currentTime > nextTime) {
+      // Serial.print(currentTime);
+      // Serial.print(", ");
+      // Serial.println(nextTime);
+      nextTime = currentTime + interval;
+      sparkleHue = (sparkleHue + 1) % 256;
+      sparkleHSVColor = CHSV(sparkleHue, SATURATION, VALUE);
+      hsv2rgb_rainbow(sparkleHSVColor, sparkleRGBColor);
+      sparkle->setColor(sparkleRGBColor);
+    }
+
+    setAll(sparkleRGBColor);
+    FastLED.show();
+    return;
+  }
+
 
   spinner->display(currentTime);
-
   sparkle->display();
 
-  // setAll(green);
   FastLED.show();
 }
 
